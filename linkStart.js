3a9b36e28f5e1e05e6b3ffdd143dae379744df76
Reed <roneal20@gmail.com>
2018-12-14 14:51:04 -0500

    fixed landing page

--- linkStart.js
+++ linkStart.js
@@ -1,26 +1,17 @@
-var $ = require('jQuery');
-
-
 let button = document.querySelector("#thisButton");
-let bodyColor = document.body.style.background = "#000500";
-let fontColor = document.body.style.color = "lime";
+
 
 
 button.addEventListener("click", function(){
     let ans = document.querySelector("#grabThis").value;
-    if(ans.includes("theinternet")){
-
+    
+    if(ans.includes("the internet")){
         document.write("deskTop");
-        bodyColor;
-        fontColor;
-   
+    }else if(ans.includes("theinternet")){
+        document.write("deskTop");
     }else{
-        document.write("wrong")
-        bodyColor;
-        fontColor;
+        document.write("invalid.");
     }
-});
 
-
-
-
+    
+})
