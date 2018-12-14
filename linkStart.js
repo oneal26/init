c1ee4f17c50b275140dfc04d4b5e344c6e1685fb
Reed <roneal20@gmail.com>
2018-12-14 14:38:56 -0500

    commiting

--- linkStart.js
+++ linkStart.js
@@ -1,10 +1,25 @@
+var $ = require('jQuery');
+
+
 let button = document.querySelector("#thisButton");
+let bodyColor = document.body.style.background = "#000500";
+let fontColor = document.body.style.color = "lime";
 
 
 button.addEventListener("click", function(){
     let ans = document.querySelector("#grabThis").value;
-    document.write(ans);
-})
+    if(ans.includes("theinternet")){
+
+        document.write("deskTop");
+        bodyColor;
+        fontColor;
+   
+    }else{
+        document.write("wrong")
+        bodyColor;
+        fontColor;
+    }
+});
 
 
 
