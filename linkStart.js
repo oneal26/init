5bfbe72476282135964f14f3329d6cdb2b358c5a
Reed <roneal20@gmail.com>
2018-12-14 10:57:09 -0500

    init

---
+++ linkStart.js
@@ -0,0 +1,12 @@
+let riddle = document.querySelector("#tomRiddle");
+let span = document.querySelector(".p")
+
+
+
+span.addEventListener("submit", function(){
+    if(span.contains("the internet")){
+        document.write("sup biiiiiiitch");
+    }else{
+        document.write("wow")
+    }
+})
